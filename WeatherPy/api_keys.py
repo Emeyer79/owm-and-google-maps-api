5e6d182ce1ba59cf0dd380a79121706153ec97f2
emeyer79 <eric.meyer79@gmail.com>
2021-03-29 18:28:34 -0500

    apikey

--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "62832213610e4c0dce680547717d03fe"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "62832213610e4c0dce680547717d03fe"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB6fwG0VO5NRZVqJtMmhICSAhXADDBiVX4"
